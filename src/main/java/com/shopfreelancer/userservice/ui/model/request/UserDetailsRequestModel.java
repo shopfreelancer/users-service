60ffff55b183494b5728dbbb5478ebc74696e447
shopfreelancer <shopfreelancer@gmail.com>
2020-01-09 15:59:25 +0100

    added postgres docker and postgres dependendy for spring boot

---
+++ src/main/java/com/shopfreelancer/userservice/ui/model/request/UserDetailsRequestModel.java
@@ -0,0 +1,12 @@
+package com.shopfreelancer.userservice.ui.model.request;
+
+import lombok.Data;
+
+@Data
+public class UserDetailsRequestModel {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
